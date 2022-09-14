7ab29c1fc5334d3987e8e181dc4dd37a7e739436
hsu <mockerybird2020@gmail.com>
2022-09-14 14:34:40 +0800

    SECOND COMMIT

--- src/main/java/com/hsu/gitidea/GitIdeaApplication.java
+++ src/main/java/com/hsu/gitidea/GitIdeaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitIdeaApplication {
 
     public static void main(String[] args) {
+        System.out.println("修改后再提交");
         SpringApplication.run(GitIdeaApplication.class, args);
     }
 
