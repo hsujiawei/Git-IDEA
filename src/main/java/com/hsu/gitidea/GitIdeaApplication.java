b6c425627893d8984ffb18d168f8b5d25878ae06
hsu <mockerybird2020@gmail.com>
2022-09-14 14:38:46 +0800

    Dev Branch Change Commit

--- src/main/java/com/hsu/gitidea/GitIdeaApplication.java
+++ src/main/java/com/hsu/gitidea/GitIdeaApplication.java
@@ -9,6 +9,7 @@ public class GitIdeaApplication {
     public static void main(String[] args) {
         System.out.println("修改后再提交");
         SpringApplication.run(GitIdeaApplication.class, args);
+        System.out.println("dev branch change...");
     }
 
 }
---
+++ src/main/java/com/hsu/gitidea/domain/User.java
@@ -0,0 +1,6 @@
+package com.hsu.gitidea.domain;
+
+public class User {
+    private String address;
+    private String name;
+}
