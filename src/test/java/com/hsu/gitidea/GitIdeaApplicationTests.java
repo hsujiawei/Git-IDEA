1e64b889677939d9e0ac435622d4922353d278ae
hsu <mockerybird2020@gmail.com>
2022-09-14 14:32:02 +0800

    FIRST COMMIT

---
+++ src/test/java/com/hsu/gitidea/GitIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsu.gitidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
